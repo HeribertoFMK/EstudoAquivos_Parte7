f5eb8e0caff3979238125f11d4b1361655de1158
Heriberto FMK <hfmk2015@gmail.com>
2021-08-17 07:13:34 +0900

    Iniciando estudo de serialização e deserialização em xml

---
+++ serializaçãoxml/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace serializaçãoxml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
