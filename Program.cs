9b894855b23ff93974c1865467bad30d22e368d4
Heriberto FMK <hfmk2015@gmail.com>
2021-08-17 09:56:01 +0900

    seguindo no estudos , agora sobre deserialização

--- Program.cs
+++ Program.cs
@@ -9,17 +9,24 @@ namespace EstudoAquivos_Parte7
     {
         static void Main(string[] args)
         {
-            Usuario usuario = new Usuario() { Name = "Jode Andrade da Costa", Cpf = "032.323.537-97", Email = "hfmk2015@gmail.com" };
-            //! a duas formas de fazer a conversão, são elas:
-            StreamWriter sw = new StreamWriter(@"C:\csharp\outro.xml");
-            StreamWriter sww = new StreamWriter(@"C:\csharp\outro1.xml");
+            // Usuario usuario = new Usuario() { Name = "Jode Andrade da Costa", Cpf = "032.323.537-97", Email = "hfmk2015@gmail.com" };
+            // //! a duas formas de fazer a conversão, são elas:
+            // StreamWriter sw = new StreamWriter(@"C:\csharp\outro.xml");
+            // StreamWriter sww = new StreamWriter(@"C:\csharp\outro1.xml");
 
 
-            XmlSerializer serializador = new XmlSerializer(typeof(Usuario));
-            XmlSerializer serializ = new XmlSerializer(usuario.GetType());
+            // XmlSerializer serializador = new XmlSerializer(typeof(Usuario));
+            // XmlSerializer serializ = new XmlSerializer(usuario.GetType());
 
-            serializador.Serialize(sw, usuario);
-            serializ.Serialize(sww, usuario);
+            // serializador.Serialize(sw, usuario);
+            // serializ.Serialize(sww, usuario);
+            StreamReader sr = new StreamReader(@"C:\csharp\outro2a.xml");
+            XmlSerializer serializando = new XmlSerializer(typeof(Usuario));
+            Usuario usuario1 = (Usuario)serializando.Deserialize(sr);
+            System.Console.WriteLine("Usuario:{0}, Cpf:{1} ,Email:{2}", usuario1.Name, usuario1.Cpf, usuario1.Email);
+
+
+
 
 
 
