dcacb34d02397d96c862218e0279597c6c29cf12
Heriberto FMK <hfmk2015@gmail.com>
2021-08-17 09:30:38 +0900

    estudos sobre serialização

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace EstudoAquivos_Parte7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Usuario usuario = new Usuario() { Name = "Jode Andrade da Costa", Cpf = "032.323.537-97", Email = "hfmk2015@gmail.com" };
+            //! a duas formas de fazer a conversão, são elas:
+            StreamWriter sw = new StreamWriter(@"C:\csharp\outro.xml");
+            StreamWriter sww = new StreamWriter(@"C:\csharp\outro1.xml");
+
+
+            XmlSerializer serializador = new XmlSerializer(typeof(Usuario));
+            XmlSerializer serializ = new XmlSerializer(usuario.GetType());
+
+            serializador.Serialize(sw, usuario);
+            serializ.Serialize(sww, usuario);
+
+
+
+        }
+    }
+}
---
+++ Usuario.cs
@@ -0,0 +1,9 @@
+namespace EstudoAquivos_Parte7
+{
+    public class Usuario
+    {
+        public string Name { get; set; }
+        public string Cpf { get; set; }
+        public string Email { get; set; }
+    }
+}
